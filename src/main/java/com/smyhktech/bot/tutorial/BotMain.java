4792e3938bd5a0a3512e48a4a8a12fd1e0fb64fb
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-01-24 18:19:52 -0800

    Initial commit

---
+++ src/main/java/com/smyhktech/bot/tutorial/BotMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smyhktech.bot.tutorial;
+
+import com.smyhktech.bot.tutorial.objects.Bot;
+
+/**
+ *
+ * @author skedzie
+ */
+public class BotMain {
+    
+    public static void main(String[] args) {
+        
+        Bot bot = new Bot("NDA1NDMyNDYwOTY5NjM5OTM2.DUrLRg.wR7TWPxkU6hbkx01CIvB-Ebdu9E");
+    }
+}
